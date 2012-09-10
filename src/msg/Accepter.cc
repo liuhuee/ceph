e52e05d4fb149485b30dd52aae66db9c18f99977
Sage Weil <sage@inktank.com>
2012-09-10 16:41:12 -0700

    msg/Accepter: fix race in accepter shutdown
    We want to avoid a race like:
    
    - entry() starts, populates pfd with listen_sd, gets past !done check
    - stop() does shutdown + close on listen_sd
    - someone else opens a new fd
    - entry() thread calls poll(2) on wrong sd
    - stop() calls join, waits forever for entry thread
    
    Also codify that stop() will never get called when listen_sd < 0 (i.e.,
    when bind() hasn't been called).
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/msg/Accepter.cc
+++ src/msg/Accepter.cc
@@ -245,12 +245,15 @@ void Accepter::stop()
 {
   done = true;
   ldout(msgr->cct,10) << "stop accepter" << dendl;
-  if (listen_sd >= 0) {
-    ::shutdown(listen_sd, SHUT_RDWR);
-    ::close(listen_sd);
-    listen_sd = -1;
-  }
+  assert(listen_sd >= 0);
+  ::shutdown(listen_sd, SHUT_RDWR);
+
+  // wait for thread to stop before closing the socket, to avoid
+  // racing against fd re-use.
   join();
+
+  ::close(listen_sd);
+  listen_sd = -1;
   done = false;
 }
 
